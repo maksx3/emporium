b5b9bffedbf74ed6f052b3e373638c2f52e6c0f8
Maksims Cekrizevs <maksx3@inbox.lv>
2023-03-07 10:34:43 +0000

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Category, Product
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
